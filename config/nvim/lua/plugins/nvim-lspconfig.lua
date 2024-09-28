3d3e44c6eee2c01a86dfdf4bdf6167f6caddb50c
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-09-28 21:35:00 +0900

    feat(neovim): add configuration of typescript language server

--- config/nvim/lua/plugins/nvim-lspconfig.lua
+++ config/nvim/lua/plugins/nvim-lspconfig.lua
@@ -1,30 +1,34 @@
 return {
-	"neovim/nvim-lspconfig",
-	event = "BufEnter",
-	config = function()
-		local lspconfig = require("lspconfig")
-		local util = require("lspconfig/util")
-		local capabilities = require("cmp_nvim_lsp").default_capabilities()
-		local on_attach = function(client)
-		end
+    "neovim/nvim-lspconfig",
+    event = "BufEnter",
+    config = function()
+        local lspconfig = require("lspconfig")
+        local util = require("lspconfig/util")
+        local capabilities = require("cmp_nvim_lsp").default_capabilities()
+        local on_attach = function(client)
+        end
 
-		lspconfig.denols.setup {}
+        lspconfig.denols.setup {
+            root_dir = util.root_pattern("deno.json"),
+        }
 
-		lspconfig.gopls.setup {}
+        lspconfig.gopls.setup {}
 
-		lspconfig.lua_ls.setup {}
+        lspconfig.lua_ls.setup {}
 
-		lspconfig.pyright.setup {
-			on_attach = on_attach,
-			capabilities = capabilities,
-			root_dir = util.root_pattern(".venv"),
-			cmd = { "bash", "-c", "source .venv/bin/activate && .venv/bin/pyright-langserver --stdio" },
-		}
+        lspconfig.pyright.setup {
+            on_attach = on_attach,
+            capabilities = capabilities,
+            root_dir = util.root_pattern(".venv"),
+            cmd = { "bash", "-c", "source .venv/bin/activate && .venv/bin/pyright-langserver --stdio" },
+        }
 
-		lspconfig.rust_analyzer.setup {}
+        lspconfig.rust_analyzer.setup {}
 
-		-- lspconfig.typos_lsp.setup {
-		--     cmd = { "/home/ts7m/programs/typos-lsp" },
-		-- }
-	end
+        lspconfig.ts_ls.setup {}
+
+        lspconfig.typos_lsp.setup {
+            cmd = { "/home/ts7m/programs/typos-lsp" },
+        }
+    end
 }
