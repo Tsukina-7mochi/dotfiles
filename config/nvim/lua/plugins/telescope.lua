225dc18722b1241fc5690af5afcac7cf8255a7d2
Tsukina-7mochi <Tsukina7mochi@gmail.com>
2024-10-01 23:26:20 +0900

    feat(nvim): show dotfiles with telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -7,10 +7,21 @@ return {
 	},
 	event = "VimEnter",
 	config = function()
+		local telescope = require('telescope')
 		local builtin = require('telescope.builtin')
+
 		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
 		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
 		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
 		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+
+		telescope.setup {
+			pickers = {
+				find_files = {
+					hidden = true,
+					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+				},
+			}
+		}
 	end,
 }
