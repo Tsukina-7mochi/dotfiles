92574871ecd5c35e36f7f010cb96a98a3d5989c3
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-03-19 14:35:23 +0900

    feat(neovim): add keymaps to telescope

--- config/nvim/lua/plugins/telescope.lua
+++ config/nvim/lua/plugins/telescope.lua
@@ -1,27 +1,39 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	tag = "0.1.8",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-telescope/telescope-fzf-native.nvim",
-	},
-	event = "VimEnter",
-	config = function()
-		local telescope = require('telescope')
-		local builtin = require('telescope.builtin')
+    "nvim-telescope/telescope.nvim",
+    tag = "0.1.8",
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+        "nvim-telescope/telescope-fzf-native.nvim",
+    },
+    event = "VimEnter",
+    config = function()
+        local telescope = require("telescope")
+        local actions = require("telescope.actions")
+        local builtin = require("telescope.builtin")
 
-		vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
-		vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-		vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
-		vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
+        vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
 
-		telescope.setup {
-			pickers = {
-				find_files = {
-					hidden = true,
-					find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
-				},
-			}
-		}
-	end,
+        telescope.setup {
+            defaults = {
+                mappings = {
+                    i = {
+                        ["<S-Enter>"] = actions.file_tab,
+                        ["<Esc>"] = actions.close,
+                    },
+                    n = {
+                        ["<S-Enter>"] = actions.file_tab,
+                    },
+                },
+            },
+            pickers = {
+                find_files = {
+                    hidden = true,
+                    find_command = { "rg", "--files", "--hidden", "--glob", "!**/.git/*" },
+                },
+            }
+        }
+    end,
 }
