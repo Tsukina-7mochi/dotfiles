7a547ca039fa4050253dc5d6d5a3aedb78df9b35
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-03-21 20:46:53 +0900

    feat(neovim): add autocmd to set lualine theme

--- config/nvim/lua/autocmd/init.lua
+++ config/nvim/lua/autocmd/init.lua
@@ -1,2 +1,3 @@
 require("autocmd.filetype")
+require("autocmd.lualine-color")
 require("autocmd.tab-width")
---
+++ config/nvim/lua/autocmd/lualine-color.lua
@@ -0,0 +1,9 @@
+vim.api.nvim_create_autocmd("ColorScheme", {
+    callback = function()
+        require("lualine").setup {
+            options = {
+                theme = "auto",
+            },
+        }
+    end,
+})
