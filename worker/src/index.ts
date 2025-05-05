d5f851bc6969256d296fdc6c5b292e335e41bdc7
Mooncake Sugar <Tsukina7mochi@gmail.com>
2025-05-05 18:55:27 +0900

    feat(worker): migrate to denoflare

--- worker/src/index.ts
+++ worker/src/index.ts
@@ -1,10 +1,16 @@
-const STATIC_ROOT = new URL('https://raw.githubusercontent.com/Tsukina-7mochi/dotfiles/refs/heads/main/');
+const STATIC_ROOT = new URL(
+  "https://raw.githubusercontent.com/Tsukina-7mochi/dotfiles/refs/heads/main/",
+);
 
 export default {
-	async fetch(request, _env, _ctx): Promise<Response> {
-		const pathname = new URL(request.url).pathname;
-		const proxyRequest = new Request(new URL(`./${pathname}`, STATIC_ROOT));
+  async fetch(
+    request: Request,
+    _env: unknown,
+    _ctx: unknown,
+  ): Promise<Response> {
+    const pathname = new URL(request.url).pathname;
+    const proxyRequest = new Request(new URL(`./${pathname}`, STATIC_ROOT));
 
-		return fetch(proxyRequest);
-	},
-} satisfies ExportedHandler<Env>;
+    return await fetch(proxyRequest);
+  },
+};
