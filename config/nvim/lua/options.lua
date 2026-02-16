4de3e646ddec558d362e83b7da7985d2595ae1ee
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-02-16 16:41:23 +0900

    style(nvim): use dot notation for vim.opt settings
    Replace bracket notation (vim.opt["key"]) with idiomatic dot notation (vim.opt.key) for all option assignments in options.lua.

--- config/nvim/lua/options.lua
+++ config/nvim/lua/options.lua
@@ -1,26 +1,26 @@
-vim.opt["autoread"] = true
-vim.opt["clipboard"] = "unnamedplus"
-vim.opt["cursorline"] = true
-vim.opt["completeopt"] = { "menuone", "noinsert", "noselect" }
-vim.opt["encoding"] = "utf-8"
-vim.opt["expandtab"] = true
-vim.opt["shell"] = "zsh"
-vim.opt["showtabline"] = 2
-vim.opt["signcolumn"] = "yes"
-vim.opt["smartindent"] = true
-vim.opt["termguicolors"] = true
-vim.opt["title"] = true
+vim.opt.autoread = true
+vim.opt.clipboard = "unnamedplus"
+vim.opt.cursorline = true
+vim.opt.completeopt = { "menuone", "noinsert", "noselect" }
+vim.opt.encoding = "utf-8"
+vim.opt.expandtab = true
+vim.opt.shell = "zsh"
+vim.opt.showtabline = 2
+vim.opt.signcolumn = "yes"
+vim.opt.smartindent = true
+vim.opt.termguicolors = true
+vim.opt.title = true
 
-vim.opt["ignorecase"] = true
-vim.opt["smartcase"] = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
 
-vim.opt["number"] = true
-vim.opt["relativenumber"] = true
-vim.opt["numberwidth"] = 4
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.numberwidth = 4
 
-vim.opt["wrap"] = false
-vim.opt["shiftwidth"] = 4
-vim.opt["tabstop"] = 4
+vim.opt.wrap = false
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
 
-vim.opt["scrolloff"] = 4
-vim.opt["sidescrolloff"] = 8
+vim.opt.scrolloff = 4
+vim.opt.sidescrolloff = 8
