c49aee02f7048b782de5d4316697c3dd08042496
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-05-12 11:36:53 +0900

    fix(nvim): explicitly specify tsserver path in ts_ls settings

--- config/nvim/after/lsp/ts_ls.lua
+++ config/nvim/after/lsp/ts_ls.lua
@@ -2,4 +2,11 @@
 return {
     root_markers = { "package.json" },
     workspace_required = true,
+    init_options = {
+      tsserver = {
+        -- Explicitly specify path for projects that node_modules are placed
+        -- not in root directory, such as pnpm workspace
+        path = vim.fs.joinpath(vim.fs.root(0, { "node_modules" }), "node_modules", "typescript", "lib")
+      },
+    },
 }
