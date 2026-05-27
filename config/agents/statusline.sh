59430ea756c0eccd393465ea5dde60566570e5a9
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-05-27 23:14:26 +0900

    feat(agents): add quota to claude code statusline

--- config/agents/statusline.sh
+++ config/agents/statusline.sh
@@ -2,7 +2,23 @@
 input=$(cat)
 
 MODEL=$(echo "$input" | jq -r '.model.display_name')
+EFFORT=$(echo "$input" | jq -r '.effort.level')
 CONTEXT=$(echo "$input" | jq -r '.context_window.used_percentage // 0' | cut -d. -f1)
+FIVE_HOUR=$(echo "$input" | jq -r '.rate_limits.five_hour.used_percentage // 0' | cut -d. -f1)
+SEVEN_DAY=$(echo "$input" | jq -r '.rate_limits.seven_day.used_percentage // 0' | cut -d. -f1)
 GIT_BRANCH=$(git symbolic-ref --short HEAD)
 
-echo "[$MODEL] ${CONTEXT}% / $GIT_BRANCH"
+braille() {
+  local p=$1
+  local levels=("⡀" "⣀" "⣄" "⣤" "⣦" "⣶" "⣷" "⣿")
+  local i=$(( p * 7 / 100 ))
+  (( i > 7 )) && i=7
+  (( i < 0 )) && i=0
+  printf '%s' "${levels[$i]}"
+}
+
+echo -n "[$MODEL / $EFFORT effort] "
+echo -n "$GIT_BRANCH / "
+echo -n "ctx ${CONTEXT}% $(braille $CONTEXT) / "
+echo -n "5h ${FIVE_HOUR}% $(braille $FIVE_HOUR) / "
+echo "7d ${SEVEN_DAY}% $(braille $SEVEN_DAY)"
