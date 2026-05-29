32f19c40df3b3b87326f37cc7fb33db7e76c08b4
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-05-30 01:01:42 +0900

    style(nvim): apply formatter

--- config/nvim/after/lsp/jsonls.lua
+++ config/nvim/after/lsp/jsonls.lua
@@ -1,6 +1,6 @@
 ---@type vim.lsp.Config
 return {
-  cmd = { "vscode-json-languageserver", "--stdio" },
+    cmd = { "vscode-json-languageserver", "--stdio" },
     settings = {
         json = {
             schemas = require("schemastore").json.schemas(),
--- config/nvim/after/lsp/ts_ls.lua
+++ config/nvim/after/lsp/ts_ls.lua
@@ -3,10 +3,10 @@ return {
     root_markers = { "package.json" },
     workspace_required = true,
     init_options = {
-      tsserver = {
-        -- Explicitly specify path for projects that node_modules are placed
-        -- not in root directory, such as pnpm workspace
-        path = vim.fs.joinpath(vim.fs.root(0, { "node_modules" }), "node_modules", "typescript", "lib")
-      },
+        tsserver = {
+            -- Explicitly specify path for projects that node_modules are placed
+            -- not in root directory, such as pnpm workspace
+            path = vim.fs.joinpath(vim.fs.root(0, { "node_modules" }), "node_modules", "typescript", "lib"),
+        },
     },
 }
