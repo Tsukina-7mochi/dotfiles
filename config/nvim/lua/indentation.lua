74da13f2fe02b53aa217301ea90f99b62faf5eb7
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-06-16 17:12:47 +0900

    feat(nvim): add autocmd to detect indentation

--- config/nvim/lua/indentation.lua
+++ config/nvim/lua/indentation.lua
@@ -1,15 +1,39 @@
-vim.api.nvim_create_user_command("UseTabIndent", function (opts)
-    local width = tonumber(opts.fargs[1])
-    vim.opt_local.expandtab = false
+---@param width integer
+---@param use_space boolean
+local function set_indentation (width, use_space)
+    vim.opt_local.expandtab = use_space
     vim.opt_local.tabstop = width
     vim.opt_local.softtabstop = width
     vim.opt_local.shiftwidth = width
+end
+
+vim.api.nvim_create_user_command("UseTabIndent", function (opts)
+    local width = tonumber(opts.fargs[1])
+    if width ~= nil then
+        set_indentation(width, false)
+    end
 end, { nargs = 1 })
 
 vim.api.nvim_create_user_command("UseSpaceIndent", function (opts)
     local width = tonumber(opts.fargs[1])
-    vim.opt_local.expandtab = true
-    vim.opt_local.tabstop = width
-    vim.opt_local.softtabstop = width
-    vim.opt_local.shiftwidth = width
+    if width ~= nil then
+        set_indentation(width, false)
+    end
 end, { nargs = 1 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function ()
+        local lines = vim.api.nvim_buf_get_lines(0, 0, 100, false)
+        for _, line in ipairs(lines) do
+            if line:match("^(\t+)") then
+                set_indentation(4, false)
+                return
+            end
+            local spaces = line:match("^( +)%S")
+            if spaces then
+                set_indentation(#spaces, false)
+                return
+            end
+        end
+    end,
+})
