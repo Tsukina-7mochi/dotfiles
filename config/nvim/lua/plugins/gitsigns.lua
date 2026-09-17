720ae04fcf5e46d08efeb7b1010cd0d82df2d29a
Mooncake Sugar <Tsukina7mochi@gmail.com>
2026-09-17 23:28:50 +0900

    feat(nvim): add git shortcuts

--- config/nvim/lua/plugins/gitsigns.lua
+++ config/nvim/lua/plugins/gitsigns.lua
@@ -4,4 +4,21 @@ return {
     config = function ()
         require("gitsigns").setup()
     end,
+    keys = {
+        {
+            "<leader>gb",
+            ":Gitsigns blame_line<Return>",
+            desc = "Git blame",
+        },
+        {
+            "<leader>gd",
+            ":Gitsigns preview_hunk_inline<Return>",
+            desc = "Git diff inline",
+        },
+        {
+            "<leader>gD",
+            ":Gitsigns diffthis<Return>",
+            desc = "Git diff",
+        },
+    },
 }
